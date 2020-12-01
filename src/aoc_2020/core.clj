cf7392af0a068c2a469464a9b0ed64dd8ec95951
Dylan Paris <dylan.paris@dylan.paris>
2020-12-01 14:31:43 -0800

    Initial commit

---
+++ src/aoc_2020/core.clj
@@ -0,0 +1,29 @@
+(ns aoc-2020.core
+  (:require [aoc-2020.day-1 :as day-1]
+            [clojure.pprint :as pp])
+  (:gen-class))
+
+(defn ^:private build-result
+  [day first-answer second-answer]
+  {"Day"                day
+   "First Star Answer"  first-answer
+   "Second Star Answer" second-answer})
+
+(defn ^:private day-1-result
+  []
+  ;; (build-result
+  ;;  1
+  ;;  (day-1/calculate-module-fuel-requirement day-1/input)
+  ;;  (day-1/calculate-total-fuel-requirement day-1/input))
+  )
+
+(defn ^:private print-result-table
+  []
+  (let [results (->> (vector (future (day-1-result)))
+                     (mapv deref))]
+    (pp/print-table ["Day" "First Star Answer" "Second Star Answer"] results)))
+
+(defn -main
+  [& args]
+  (time
+   (print-result-table)))
---
+++ src/aoc_2020/day_1.clj
@@ -0,0 +1,2 @@
+(ns aoc-2020.day-1
+  (:require [clojure.java.io :as io]))
